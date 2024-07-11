86006ca97855617be414e14e92cb6f380ee3dffd
Devnol <pgkoutsoumanis@gmail.com>
2024-07-11 16:51:55 +0300

    Make solid line and dashed line border generators
    Signed-off-by: Devnol <pgkoutsoumanis@gmail.com>

--- art/borderinator-Devnol/project.js
+++ art/borderinator-Devnol/project.js
@@ -4,40 +4,64 @@
 @snapshot: border_sample1.png
 */
 
-const width = 75;
+const width = 75; //size of a post-it note because blot can't fit a real piece of paper :(
 const height = 75;
 
 setDocDimensions(width, height);
 
-// store final lines here
-const finalLines = [];
-
 //border padding from paper edge
 const padding = 2;
 
-// generate border
+// generate basic border
 const border = [
-    [0+padding, 0+padding],
-    [0+padding, height-padding],
-    [width-padding, height-padding],
-    [width-padding, 0+padding],
-    [0+padding, 0+padding]
+  [0, 0],
+  [0, height],
+  [width, height],
+  [width, 0],
+  [0, 0]
 ]
 
-// store all border polylines
-let B = [border]
-// let B = []
-bt.resample([border], 0.1) //make border HD
+bt.resample([border], 0.1); //make border HD
 
-// bt.rotate([border], 10)
-// for (let i = 1; i<8; i+=2) {
-  // B.push(bt.trim([border], i/10, (i+1)/10));
-// }
-drawLines(
-    [bt.trim(bt.copy([border]), 0.1, 0.5),
-    bt.trim(bt.copy([border]), 0.6, 0.9)]
-     
-)
-B.push(border)
-// draw it
-drawLines(B);
+//function to add padding to a border, pad is distance in units from the edge of the doc
+function padBorder(border, pad) {
+  bt.scale(border, [(width - 2 * pad) / width, (height - 2 * pad) / height], [height / 2, width / 2]);
+}
+
+function frameMod(thickness, color) {
+  return 
+}
+
+/*
+generate border of n unit segments, 
+of which the line segments are of dashLen and 
+the gaps between them of gapLen relative length
+*/
+function dashedFrame(n, dashLen, gapLen) {
+  let frame = [];
+  for (let i = 0; i < n; i += (gapLen + dashLen)) {
+    bt.join(frame, bt.trim(bt.copy([border]), i / n, (i + dashLen) / n));
+  }
+  return frame;
+}
+/*
+function zigzagFrame(n, angle) {
+  let frame = [];
+  for (let i = 0; i < n; i++) {
+    let segment = bt.trim(bt.copy([border], i/n, (i+2)/n));
+    if (n%2) {
+      bt.rotate(segment, angle);
+    }
+    else {
+      bt.rotate(segment, -angle);
+    }
+  }
+}
+*/
+const frame1 = dashedFrame(100,1,1);
+padBorder(frame1, 2);
+drawLines(frame1);
+
+const frame2 = zigzagFrame(100,30);
+padBorder(frame2, 4);
+drawLines(frame2, {width:4, stroke:"#7289da"});
